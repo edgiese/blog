63f5a0f148542ca595042fd55d6e2977409714fe
Ed Giese <edgiese@gmail.com>
2021-06-05 00:22:36 +0000

    remove parse

--- components/Layout.js
+++ components/Layout.js
@@ -19,7 +19,6 @@ export default function Layout({ children, pageTitle, description, setup_comment
             js.nonce = 'vQTDnsLQ'
             window.ed_js=js
             fjs.parentNode.insertBefore(js, fjs);
-            window.FB.XFBML.parse();
         }
     }
     // noinspection CheckTagEmptyBody
